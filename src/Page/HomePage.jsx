41def742de9f46fb0f31fdaf6f2b5af08d0044e9
ratulislam46 <arfanratul46@gmail.com>
2026-04-14 11:31:16 +0600

    Added Testimonials section

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Testimonials from './Testimonials';
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+           <Testimonials/>
         </div>
     );
 };
